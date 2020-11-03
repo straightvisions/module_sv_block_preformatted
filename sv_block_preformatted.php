ea0445b629d902b5263365352067c7455ead9247
Matthias Bathke <matthias-bathke@straightvisions.com>
2020-11-03 15:29:48 +0100

    init

---
+++ sv_block_preformatted.php
@@ -0,0 +1,76 @@
+<?php
+	namespace sv100;
+
+	class sv_block_preformatted extends init {
+		public function init() {
+			$this->set_module_title( __( 'Block: Preformatted', 'sv100' ) )
+				->set_module_desc( __( 'Settings for Gutenberg Block', 'sv100' ) )
+				->set_css_cache_active()
+				->set_section_title( $this->get_module_title() )
+				->set_section_desc( $this->get_module_desc() )
+				->set_section_type( 'settings' )
+				->set_section_template_path()
+				->set_section_order(5000)
+				->get_root()
+				->add_section( $this );
+		}
+		
+		protected function load_settings(): sv_block_preformatted {
+			$this->get_setting( 'font' )
+				->set_title( __( 'Font Family', 'sv100' ) )
+				->set_description( __( 'Choose a font for your text.', 'sv100' ) )
+				->set_options( $this->get_module( 'sv_webfontloader' ) ? $this->get_module( 'sv_webfontloader' )->get_font_options() : array('' => __('Please activate module SV Webfontloader for this Feature.', 'sv100')) )
+				->set_is_responsive(true)
+				->load_type( 'select' );
+
+			$this->get_setting( 'font_size' )
+				->set_title( __( 'Font Size', 'sv100' ) )
+				->set_description( __( 'Font Size in pixel.', 'sv100' ) )
+				->set_default_value( 16 )
+				->set_is_responsive(true)
+				->load_type( 'number' );
+
+			$this->get_setting( 'line_height' )
+				->set_title( __( 'Line Height', 'sv100' ) )
+				->set_description( __( 'Set line height as multiplier or with a unit.', 'sv100' ) )
+				->set_is_responsive(true)
+				->load_type( 'text' );
+
+			$this->get_setting( 'text_color' )
+				->set_title( __( 'Text Color', 'sv100' ) )
+				->set_default_value( '#1e1e1e' )
+				->set_is_responsive(true)
+				->load_type( 'color' );
+
+			$this->get_setting( 'margin' )
+				->set_title( __( 'Margin', 'sv100' ) )
+				->set_is_responsive(true)
+				->load_type( 'margin' );
+
+			$this->get_setting( 'padding' )
+				->set_title( __( 'Padding', 'sv100' ) )
+				->set_is_responsive(true)
+				->load_type( 'margin' );
+
+			$this->get_setting( 'border' )
+				->set_title( __( 'Border', 'sv100' ) )
+				->set_is_responsive(true)
+				->load_type( 'border' );
+
+			return $this;
+		}
+		public function enqueue_scripts(): sv_block_preformatted {
+			if(!$this->has_block_frontend('preformatted')){
+				return $this;
+			}
+
+			if(!is_admin()){
+				$this->load_settings()->register_scripts();
+			}
+			
+			$this->get_script( 'common' )->set_is_enqueued();
+			$this->get_script( 'config' )->set_is_enqueued();
+			
+			return $this;
+		}
+	}
