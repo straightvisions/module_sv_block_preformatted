722e5ac26262f48109da24c6bceaccd80ed86ef1
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-07-26 16:25:35 +0200

    fix selector

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-preformatted' : '.wp-block-preformatted',
+		'.wp-block-preformatted',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
